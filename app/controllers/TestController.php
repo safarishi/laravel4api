7a7306bd0bb58c569db031f8ed26db2d441017f2
safari <sshsfl@yeah.net>
2015-11-13 13:10:44 +0800

    新增 测试控制器

--- app/controllers/TestController.php
+++ app/controllers/TestController.php
@@ -1,22 +1,34 @@
 <?php
 
+use LucaDegasperi\OAuth2Server\Authorizer;
+use Rootant\Api\Exception\ValidationException;
+
 class TestController extends CommonController
 {
+    /**
+     * [__construct description]
+     *
+     */
     public function __construct(Authorizer $authorizer)
     {
         parent::__construct($authorizer);
-        $this->beforeFilter('oauth', ['except' => ['anonymousReply']]);
+        $this->beforeFilter('oauth', ['except' => ['functionName', 'test']]);
         $this->beforeFilter('validation');
-        $this->beforeFilter('@prepare', ['only' => ['reply', 'anonymousReply', 'favour', 'unfavour']]);
-        $this->afterFilter('disconnect:major', ['only' => ['reply', 'anonymousReply', 'favour', 'unfavour']]);
+        $this->afterFilter('disconnect:major', ['only' => ['functionName', 'test']]);
     }
 
     private static $_validate = [
-        'reply' => [
-            'content' => 'required',
-        ],
-        'anonymousReply' => [
-            'content' => 'required',
-        ],
+        // todo
+        // 格式类似如下
+        // 'functionName' => [
+            // 'username' => 'required|min:5|unique:user,name',
+            // 'email'    => 'required|email|unique:user',
+            // 'password' => 'required|min:6|confirmed',
+        // ],
     ];
+
+    public function test()
+    {
+        // code ...
+    }
 }
